e0d018268b618fcaa8f54240ee9acd8dbffa2473
Lukáš Rác <lukiss482@gmail.com>
2019-07-22 19:34:47 +0200

    22/07/2019

--- Promises/app.js
+++ Promises/app.js
@@ -6,11 +6,9 @@ const getTodos = (resource) => {
         request.addEventListener("readystatechange", () => {
             if (request.readyState === 4 && request.status === 200) {
                 const data = JSON.parse(request.responseText);
-                //console.log(undefined, data);    // způsob pomocí Callbacku
-                resolve(data);                     // způsob pomocí Promises
+                resolve(data);
             } else if (request.readyState === 4) {
-                //console.log('Could not fetch data', data);  // způsob pomocí Callbacku 
-                reject("Error getting resource");             // způsob pomocí Promises
+                reject("Error getting resource");
             }
         });
         request.open("GET", resource);
@@ -30,34 +28,3 @@ getTodos("todos/lukas.json").then(data => {
 }).catch(err => {
     console.log("Promise rejected: ", err);
 });
-
-// Callback 
-/*getTodos("todos/lukas.json", (err, data) => {
-    console.log(data);
-    getTodos("todos/honza.json", (err, data) => {
-        console.log(data);
-        getTodos("todos/tomas.json", (err, data) => {
-            console.log(data);
-        });
-    });
-});*/
-
-// Promises
-/* const getSomething = () => {
-    return new Promise((resolve, reject) => {
-        //resolve("some data");
-        reject("some error");
-    });
-}; */
-
-/* getSomething().then(data => {
-    console.log(data);
-}, (err) => {
-    console.log(err);
-}); */
-
-/* getSomething().then(data => {
-    console.log(data);
-}).catch(err => {
-    console.log(err);
-}); */
