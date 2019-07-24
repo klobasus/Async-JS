10d8b9cb00531d499853ac2159002da3953aceb2
Lukáš Rác <lukiss482@gmail.com>
2019-07-24 22:11:55 +0200

    24/07/2019

--- Async Await/app.js
+++ Async Await/app.js
@@ -1,14 +1,12 @@
 const getTodos = async resource => {
-
-    const response = await fetch(resource);
-    if(response.status !== 200){
-        throw new Error('Could not fetch data');
-    };
-    const data = response.json();
-    return data;
-
+  const response = await fetch(resource);
+  if (response.status !== 200) {
+    throw new Error('Could not fetch data');
+  }
+  const data = response.json();
+  return data;
 };
 
 getTodos('../todos/lukas.json')
-    .then(data => console.log(data))
-    .catch(err => console.log(err.message));
+  .then(data => console.log(data))
+  .catch(err => console.log(err.message));
